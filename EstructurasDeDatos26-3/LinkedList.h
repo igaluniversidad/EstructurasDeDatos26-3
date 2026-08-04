3daea37fc60fbfe77cb5961bcff36ac71ac5b8c6
Carlo Hinojosa Relión <wowperrodevr@gmail.com>
2026-08-03 18:59:13 -0600

    LinkListh

---
+++ EstructurasDeDatos26-3/LinkedList.h
@@ -0,0 +1,16 @@
+#pragma once
+class LinkedList
+{
+private:
+	int _size;
+
+public:
+	LinkedList();
+	void Add(int dato);
+	int GetAt(int index);
+	int GetSize();
+	void Print();
+	void InsertionSort();
+	~LinkedList();
+};
+
